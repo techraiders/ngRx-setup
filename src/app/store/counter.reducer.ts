ccf977d35529cff0b20f73475378974661c30475
Navneet Prakash <hello_navneet@hotmail.com>
2023-08-14 15:53:37 +0530

    Defines increment action, and dispatches.

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,8 +1,11 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { increment } from "./counter.actions";
 
 const initialState = 0; // value could be any primitive or no-premitive.
 
-export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(initialState,
+  on(increment, state => ++state)
+);
 
 // An older alternative to create reducer
 // export function counterReducer(state = initialState) {
