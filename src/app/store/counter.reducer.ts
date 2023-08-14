ee2a38d3ba2013bd15a77d6cb7bfc986d408a427
Navneet Prakash <hello_navneet@hotmail.com>
2023-08-14 16:15:57 +0530

    Handles action without createReducer.

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,11 +3,17 @@ import { increment } from "./counter.actions";
 
 const initialState = 0; // value could be any primitive or no-premitive.
 
-export const counterReducer = createReducer(initialState,
-  on(increment, (state, action) => state + action.value)
-);
+// export const counterReducer = createReducer(initialState,
+//   on(increment, (state, action) => state + action.value)
+// );
 
 // An older alternative to create reducer
-// export function counterReducer(state = initialState) {
-//   return initialState;
-// }
+export function counterReducer(state = initialState, action: any) {
+  switch (action.type) {
+    case `[Counter] Increment`:
+      return state + action.value;
+
+    default:
+      return state;
+  }
+}
