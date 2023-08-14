cba22477b0ed4bc313a05da2d497f7b1e03735c2
Navneet Prakash <hello_navneet@hotmail.com>
2023-08-14 12:21:14 +0530

    Installs @ngRx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
@@ -11,7 +12,7 @@ import { CounterControlsComponent } from './counter-controls/counter-controls.co
     CounterOutputComponent,
     CounterControlsComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
